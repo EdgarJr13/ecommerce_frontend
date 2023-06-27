99a47530f576c2774223449ea688a0ce5eba9956
Edgar Pinheiro <edgarpinheiro13@gmail.com>
2023-06-27 17:43:57 -0300

    adição de novos ícones e pequenas alterações na estrutura das páginas.

--- src/components/GerenciarProdutos.vue
+++ src/components/GerenciarProdutos.vue
@@ -1,24 +1,30 @@
 <template>
-  <v-container>
-    <h1 class="text-center">Lista de Produtos</h1>
-    <v-row justify="center">
-      <v-col cols="12" sm="8" md="6" v-for="produto in produtos" :key="produto.id">
-        <v-card class="mb-4">
-          <v-card-title>{{ produto.nome }}</v-card-title>
-          <v-card-text>
-            <p>{{ produto.descricao }}</p>
-            <p>{{ produto.valor }}</p>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="primary" @click="adicionarAoCarrinho(produto)">
-              <v-icon left>mdi-cart</v-icon>
-              Adicionar ao Carrinho
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="gerenciar-produtos-page">
+    <v-container>
+      <h1 class="text-center">Produtos em estoque</h1>
+      <v-row justify="center">
+        <v-col cols="12" sm="8" md="6" v-for="produto in produtos" :key="produto.id">
+          <v-card class="mb-4">
+            <v-card-title>{{ produto.nome }}</v-card-title>
+            <v-card-text>
+              <p>{{ produto.descricao }}</p>
+              <p>R${{ produto.valor }},00</p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" @click="editarProduto(produto)">
+                <v-icon left>mdi-pencil</v-icon>
+                Editar
+              </v-btn>
+              <v-btn color="primary" @click="excluirProduto(produto.id)">
+                <v-icon left>mdi-delete</v-icon>
+                Deletar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -59,3 +65,9 @@ export default {
     }
 };
 </script>
+
+<style>
+.gerenciar-produtos-page {
+  margin-top: 70px;
+}
+</style>
--- src/components/ListaProdutos.vue
+++ src/components/ListaProdutos.vue
@@ -2,8 +2,8 @@
     <div class="lista-produtos-page">
         <v-container>
             <v-row>
-                <v-col v-for="produto in produtos" :key="produto.id" cols="12" sm="6" md="4" lg="3">
-                    <v-card>
+                <v-col v-for="produto in produtos" :key="produto.id" cols="12" sm="8" md="6">
+                    <v-card class="mb-4">
                         <v-card-title>{{ produto.nome }}</v-card-title>
                         <v-card-text>{{ produto.descricao }}</v-card-text>
                         <v-card-text>R${{ produto.valor }},00</v-card-text>
@@ -16,15 +16,6 @@
                 </v-col>
             </v-row>
         </v-container>
-        <!-- <h2>Lista de Produtos</h2>
-        <ul>
-            <li v-for="produto in produtos" :key="produto.id">
-                <h3>{{ produto.nome }}</h3>
-                <p>{{ produto.descricao }}</p>
-                <p>{{ produto.valor }}</p>
-                <button @click="adicionarAoCarrinho(produto)">Adicionar ao Carrinho</button>
-            </li>
-        </ul> -->
     </div>
   </template>
   
